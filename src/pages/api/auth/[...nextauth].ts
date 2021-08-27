a2a91df670a8d9da6a10560e2970f093afe2de01
Bruno Zutim <bzutim@gmail.com>
2021-08-27 10:15:49 -0300

    refactor: get oauth account id

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
 export default NextAuth({
+
   providers: [
     Providers.Facebook({
       clientId: process.env.FACEBOOK_ID,
@@ -12,6 +13,24 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET
     }),
   ],
+
+  callbacks: {
+    async signIn(user, account, profile) {
+      if (account.provider === 'google' && account.id) {
+
+        console.log('google_id', account.id);
+
+        return true;
+      } else if (account.provider === 'facebook' && account.id) {
+
+        console.log('facebook_id', account.id);
+
+        return true;
+      } else {
+        return false;
+      }
+    },
+  }
   // Optional SQL or MongoDB database to persist users
   //database: process.env.DATABASE_URL
 });
